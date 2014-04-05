bfdf10de225be72125bab5866d272f3b8aa624be
sycat <sycat@sto.tw>
2014-04-06 00:58:46 +0800

    Test LINQ.

--- Methods/Module1.vb
+++ Methods/Module1.vb
@@ -7,9 +7,21 @@ Module Module1
     Sub Main()
 
         Try
-            Dim date1 As DateTime = Date.Now.Date
-            Dim date2 As DateTime = DateTime.Parse("16MAR14", System.Globalization.CultureInfo.InvariantCulture)
-            Console.WriteLine(Date.Now.ToString())
+            Dim contactDoc As XDocument = _
+                <?xml version="1.0"?>
+                <contact>
+                    <name>Patrick Hines</name>
+                    <phone type="home">206-555-0144</phone>
+                    <phone type="work">425-555-0145</phone>
+                </contact>
+
+            Dim xdocument As XDocument = xdocument.Load("..\..\Test.xml")
+            Dim employees As IEnumerable(Of XElement) = contactDoc.Elements()
+            ' Read the entire XML
+            For Each employee In employees
+                Console.WriteLine(employee)
+            Next
+
 
 
             Console.WriteLine("fin")
